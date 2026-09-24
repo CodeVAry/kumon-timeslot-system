2ac8b5050fe291b0e8db59d0d7cdf11437fe9196
Ariyan Chowdhury <41270214+Ariyanchy@users.noreply.github.com>
2026-09-25 02:03:47 +1000

    import allies

--- app/Models/Admin/StudentImportAlias.php
+++ app/Models/Admin/StudentImportAlias.php
@@ -15,3 +15,28 @@ class StudentImportAlias extends Model
         'target_external_id',
     ];
 }
+<?php
+
+namespace App\Models\Admin;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class StudentImportAlias extends Model
+{
+    protected $table = 'student_import_aliases';
+
+    protected $fillable = [
+        'source_name',
+        'target_external_id',
+    ];
+
+    public function student(): BelongsTo
+    {
+        return $this->belongsTo(
+            Student::class,
+            'target_external_id',
+            'external_id'
+        );
+    }
+}
